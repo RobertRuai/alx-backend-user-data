c273cc2e0b7b9ceb1aab20d49df800888a49dbd4
RobertRuai <robertruai4@gmail.com>
2023-11-03 15:33:03 +0300

    validate that the provided password matches the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,12 @@ def hash_password(password: str) -> bytes:
     """returns a salted, hashed password"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(
+             hashed_password: bytes,
+             password: str) -> bool:
+    """validates provided pswd matches hashed pswd"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
