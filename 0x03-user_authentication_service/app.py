a20c3de997ba6c2a9d53a81aa8761b47dafabaaa
RobertRuai <robertruai4@gmail.com>
2023-11-14 15:42:04 +0300

    a users function that implements the POST /users route to register new user.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """ Flask Application """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def message():
     """ returns json string message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """registers user if thtey dont exist"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
