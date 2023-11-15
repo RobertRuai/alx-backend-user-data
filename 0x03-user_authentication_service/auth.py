f02eb418b28d07c65a852abeb2e652e0b5a099b1
RobertRuai <robertruai4@gmail.com>
2023-11-15 17:27:29 +0300

    takes a single session_id string argument and returns the corresponding User or None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,7 +57,7 @@ class Auth:
         except NoResultFound:
             return
 
-    def get_user_from_session_id(session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> User:
         """ returns the corresponding User or None"""
         if session_id:
             try:
