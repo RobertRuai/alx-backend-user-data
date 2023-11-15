8d1946aa5256203e5864a19cd7b68ad09b21f664
RobertRuai <robertruai4@gmail.com>
2023-11-15 18:45:50 +0300

    profile function to respond to the GET /profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,6 +56,19 @@ def logout():
     abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """responds to the GET /profile route"""
+    sesh_id = request.cookies.get('session_id', None)
+    if sesh_id is not None:
+        user = AUTH.get_user_from_session_id(sesh_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+        else:
+            abort(403)
+    abort(403)
+
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host="0.0.0.0", port="5000")
