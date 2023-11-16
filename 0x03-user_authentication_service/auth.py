67767b0ebe89bfc316da9f3915ae6c905835c704
RobertRuai <robertruai4@gmail.com>
2023-11-16 19:17:42 +0300

    ash the password and update the user’s hashed_password field with the new hashed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,3 +83,14 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update the password with a token. """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
+        except Exception:
+            raise ValueError
+        return None
